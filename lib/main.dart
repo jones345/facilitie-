a42f9b25810a7c7567b1d98d8c58cb00b9442fe1
zntongana@bitri.co.bw <Jones321Q10>
2021-05-02 11:17:42 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'homepage.dart';
+import 'secondpage.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+void main() {
+  runApp(   DevicePreview(
+    builder: (context)=> MyApp(),
+    enabled: !kReleaseMode,
+  )
+  );
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // builder: (context, widget)=> ResponsiveWrapper.builder(
+      //   BouncingScrollWrapper.builder(context, widget),
+      //   maxWidth:1200,
+      //   minWidth:450,
+      //   defaultScale: true,
+      //   breakpoints: [
+      //     ResponsiveBreakpoint(breakpoint: 450, name: MOBILE),
+      //     ResponsiveBreakpoint(
+      //           breakpoint: 800, name: TABLET, autoScale: true),
+      //       ResponsiveBreakpoint(
+      //           breakpoint: 1000, name: TABLET, autoScale: true),
+      //       ResponsiveBreakpoint(breakpoint: 1200, name: DESKTOP),
+      //       ResponsiveBreakpoint(breakpoint: 2460, name: "4K", autoScale: true),
+      //   ]
+      // ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
